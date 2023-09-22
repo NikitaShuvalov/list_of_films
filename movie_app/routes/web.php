7ecf2310437656b056e7963c12e662dcf2feb428
NikitaShuvalov <ktototam065@gmail.com>
2023-09-22 16:08:17 +0600

    added routes get movies and get movies by id.

--- movie_app/routes/web.php
+++ movie_app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("/movies", [MovieController::class, "index"]);
+
+Route::get("/movies/{id}", [MovieController::class, "show"]);
